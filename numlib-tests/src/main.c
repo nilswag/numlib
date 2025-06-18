684e5ff96e2625b6ea4dd346cf70b58b95924706
nilswag <nilswagemaker05@icloud.com>
2025-06-18 17:17:56 +0200

    added project setups

---
+++ numlib-tests/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include <add.h>
+
+int main(void)
+{
+	printf("%f\n", add(1, 2));
+	return 0;
+}
---
+++ numlib/src/add.c
@@ -0,0 +1,6 @@
+#include "add.h"
+
+double add(double a, double b)
+{
+	return a + b;
+}
