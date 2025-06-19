0a9a1b85599325646ac8b53108ec078b5766a4e4
nilswag <nilswagemaker05@icloud.com>
2025-06-19 23:49:47 +0200

    Added general matrix multiplication algorithm

--- numlib/include/core/mat.h
+++ numlib/include/core/mat.h
@@ -8,6 +8,13 @@
  * @param aw - The amount of columns of matrix a.
  * @param bw - The amount of rows of matrix b.
  * @param bh - The amount of columns of matrix b.
+ * 
+ * @param a - Pointer to the array of matrix a.
+ * @param b - Pointer to the array of matrix b.
+ * @param c - Pointer to the array of result matrix c.
+ * 
+ * @return numlib_error - NUMLIB_SUCCESS on success, or
+ *      NUMLIB_ERR_SIZE_MISMATCH on incompatible matrix size.
  */
 numlib_error numlib_matf_multiply(
     int ah,
---
+++ numlib/src/core/mat.c
@@ -0,0 +1,32 @@
+#include "core/mat.h"
+
+numlib_error numlib_matf_multiply(
+    int ah,
+    int aw,
+    int bh,
+    int bw,
+    float* a,
+    float* b,
+    float* c
+) {
+    if (aw != bh) return NUMLIB_ERR_SIZE_MISMATCH;
+    /*
+        A: ah x aw (M x N)
+        B: bh x bw (N x P) -> ah = bw
+        C: ah x bw (M x N)
+    */
+    for (int i = 0; i < ah; i++) // rows (a & c)
+    {
+        for (int j = 0; j < bw; j++) // columns (b & c)
+        {
+            float sum = 0.0f; // reset value in c
+            for (int k = 0; k < aw; k++)
+            {
+                //     a[i][k]      *  b[k][j]
+                sum += a[i * aw + k] * b[k * bw + j];
+            }
+            c[i * bw + j] = sum;
+        }
+    }
+    return NUMLIB_SUCCESS;
+}
